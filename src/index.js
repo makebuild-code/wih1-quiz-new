61cc1589383b617e7ce4de968ced76b9cbd2ecae
elisa-sanchez-mb <elisa@makebuild.studio>
2026-03-04 13:29:29 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { nav } from './modules/nav.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  nav()
+
+  console.log('this is liams console log');
+  console.log('this is a demo - new line from df');
+  console.log('hello james');
+});
